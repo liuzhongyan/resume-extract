a38fb1911f89ced6dc1748737699400e6b3e2be4
liuzhongyan <1123460291@qq.com>
2022-12-27 20:22:03 +0800

    Initial commit

---
+++ src/main/java/com/info/resume/ResumeExtractApplication.java
@@ -0,0 +1,13 @@
+package com.info.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeExtractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeExtractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/info/resume/ResumeExtractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeExtractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
